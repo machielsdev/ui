7769999cc071899a94af5b935bb47c9747dca964
Eran Machiels <dev@eranmachiels.nl>
2020-10-26 20:29:08 +0100

    Added basic buttons

--- src/components/utils/Variant.ts
+++ src/components/utils/Variant.ts
@@ -1,2 +1,15 @@
 export enum Variant {
+    PRIMARY = 'primary',
+    PRIMARY_GHOST = 'primary-ghost',
+    SECONDARY = 'secondary',
+    DANGER = 'danger',
+    DANGER_GHOST = 'danger-ghost'
 }
+
+export const variantList: string[] = [
+    'primary',
+    'primary-ghost',
+    'secondary',
+    'danger',
+    'danger-ghost'
+];
